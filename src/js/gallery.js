76fd725c158edcc624b89823222a23fe02639803
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-12-10 20:18:16 +0200

    add styles and btn more

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -23,9 +23,13 @@ async function onSubmit(evt) {
     try {
         const { data } = await getPhotos(page, query)
         const { totalHits, hits, total } = data;
+        if (!searchQuery) {
+            return Notiflix.Notify.failure('Please enter a search query.')
+        }  
         if (hits.length === 0) {
             return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-        }        divGalleryEl.innerHTML=createMarkup(hits);    
+        }    
+        divGalleryEl.innerHTML=createMarkup(hits);    
         lightbox.refresh();
 Notiflix.Notify.success(`Hooray! We found ${total} images.`)
         if (totalHits > perPage) {
@@ -36,27 +40,53 @@ Notiflix.Notify.success(`Hooray! We found ${total} images.`)
     }
 }
 
+async function onclick() {
+    page += 1;
+    try {
+        const { data } = await getPhotos(page, query)
+        const { totalHits, hits } = data;
+        divGalleryEl.insertAdjacentHTML('beforeend', createMarkup(hits))
+        lightbox.refresh();
 
+                // Плавне прокручування сторінки
+        const { height: cardHeight } = document
+            .querySelector(".gallery")
+            .firstElementChild.getBoundingClientRect();
+
+        window.scrollBy({
+            top: cardHeight * 2,
+            behavior: "smooth",
+        });
+
+        const lastPage = Math.ceil(totalHits / perPage);
+        if (lastPage === page) {
+            moreBtnEl.classList.add('is-hidden')
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
+        } 
+    } catch(error) {
+        console.log(error);
+    }
+}
 
 function createMarkup(arr) {
     return arr.map(item => `<div class="photo-card">
     <a class="gallery-link" href="${item.largeImageURL}">
     <img class="gallery-image" src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
-    </a>
     <div class="info">
-      <p class="info-item">
-        <b>Likes</b> ${item.likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b> ${item.views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b> ${item.comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b> ${item.downloads}
-      </p>
+    <p class="info-item">
+    <b>Likes</b> ${item.likes}
+    </p>
+    <p class="info-item">
+    <b>Views</b> ${item.views}
+    </p>
+    <p class="info-item">
+    <b>Comments</b> ${item.comments}
+    </p>
+    <p class="info-item">
+    <b>Downloads</b> ${item.downloads}
+    </p>
     </div>
+    </a>
   </div>`).join('')
 }
 
